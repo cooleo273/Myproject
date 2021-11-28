7f3cbd26aae08ca66314148714cf95c993d770c4
cooleo273 <leulteferi273@gmail.com>
2021-11-28 07:00:52 +0000

    Creating Buttons

---
+++ src/components/buttons/ordernow/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const OrderNow =()=>{
+    return(
+        <button>Order Now</button>
+    )
+}
+
+export default OrderNow;
---
+++ src/components/buttons/view more/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ViewMore =()=>{
+    return(
+        <button>View more</button>
+    )
+}
+
+export default ViewMore;
