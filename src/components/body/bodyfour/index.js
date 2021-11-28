44f8879122be5f0ac7e49c3fdd13d97096c55069
cooleo273 <leulteferi273@gmail.com>
2021-11-28 15:53:14 +0000

    Forming Body Four

---
+++ src/components/body/bodyfour/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ViewMore from "../../buttons/view more";
+import "./styles.css";
+const BodyFour = () => {
+    return (
+        <div className="bodyfourcontainer">
+            <div>
+                <h1>People say about Beeanter</h1>
+                <p>all from clients, we display here so you can see</p>
+            </div>
+            <div className="feedbackcontainer">
+                <div className="feedbackcontainerone">
+                    <div className="firstperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+                    <div className="secondperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+                    <div className="thirdperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+                </div>
+                <div className="feedbackcontainertwo">
+                    <div className="fourthperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+                    <div className="fifthperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+                    <div className="sixthhperson">
+                        <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BodyFour;
