3825ca432d81fff6116e52b56465c83c00e749ee
cooleo273 <leulteferi273@gmail.com>
2021-11-29 09:22:22 +0000

    added and styled pictures in Body four

--- src/components/body/bodyfour/index.js
+++ src/components/body/bodyfour/index.js
@@ -1,5 +1,10 @@
 import React from "react";
-import ViewMore from "../../buttons/view more";
+import img from"./first person.jpg"
+import img1 from"./second person.jpg"
+import img2 from "./third person.jpg"
+import img3 from"./fourth person.jpg"
+import img4 from"./fifth person.jpg"
+import img5 from"./sixth person.jpg"
 import "./styles.css";
 const BodyFour = () => {
     return (
@@ -12,28 +17,73 @@ const BodyFour = () => {
                 <div className="feedbackcontainerone">
                     <div className="firstperson">
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="firstpersoninfo">
+                            <img src={img} alt="alexandro" ></img>
+                            <div className="firstpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
+                        </div>
                     </div>
                     <div className="secondperson">
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="secondpersoninfo">
+                            <img src={img1} alt="alexandro" ></img>
+                            <div className="secondpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
+                        </div>
                     </div>
                     
                 </div>
                 <div className="feedbackcontainertwo">
                 <div className="thirdperson">
+
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="thirdpersoninfo">
+                            <img src={img2} alt="alexandro"></img>
+                            <div className="thirdpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
+                        </div>
                     </div>
                     <div className="fourthperson">
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="fourthpersoninfo">
+                            <img src={img3} alt="alexandro"></img>
+                            <div className="fourthpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
                     </div>
+                   
+                        </div>
                    
 
                 </div>
                 <div className="feedbackcontainerthree">
                 <div className="fifthperson">
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="fifthpersoninfo">
+                            <img src={img4} alt="alexandro"></img>
+                            <div className="fifthpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
+                        </div>
                     </div>
+
                     <div className="sixthperson">
                         <p>The application is very charming, the minimalist design makes users not confused when using it</p>
+                        <div className="sixthpersoninfo">
+                            <img src={img5}  alt="alexandro"></img>
+                            <div className="sixthpersonName">
+                                 <h3>Alexandro</h3>
+                                  <p>Programmer <span>@metaku</span></p>  
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
