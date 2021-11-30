afdff42e75f63cda652dbd0204e15959017bfe73
cooleo273 <leulteferi273@gmail.com>
2021-11-30 04:57:03 +0000

    Modifying body five

--- src/components/body/bodyfive/index.js
+++ src/components/body/bodyfive/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import img from "./rishabh-malhotra-83ypHTv6J2M-unsplash.jpg"
 import "./styles.css"
 
 const BodyFive = () => {
     return (
         <div className="bodyfivecontainer">
-            <div class="flex social-btns">
+            <div className="bodyfivetextcontainer">
+            <h1>Dowload and use it on your phone</h1>
+            <span className="text">Immediatly download Beeanter for free on your cellphone, don't let you miss the promo promos
+                that are offered because we always share interesting promos
+            </span>
+             <div class="flex social-btns">
                 <a class="app-btn blu flex vert" href="http:apple.com">
                     <i class="fab fa-apple"></i>
                     <p>Available on the <br /> <span class="big-txt">App Store</span></p>
@@ -12,7 +18,13 @@ const BodyFive = () => {
                 <a class="btn btn-google" href="#" title="Google Play">Google Play</a>
 
             </div>
-        </div>
+            </div>
+            <div className="image">
+                <img src={img} alt="Phone"/>
+            </div>  
+            </div>
+            
+        
             )
 }
 export default BodyFive;
